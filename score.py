7f1415a2f131477abd25d4ae6b4d8eff4566301c
HP <khoinguyen17062002@gmail.com>
2018-08-21 22:28:03 +0700

    bla

--- score.py
+++ score.py
@@ -1,9 +1,17 @@
 import pygame
+width = 800
+height = 640
+display_surf = pygame.display.set_mode((width, height))
 class ScoreBoard:
-    def __init__(self, score):
+    def __init__(self, x, y, score, size):
+        self.x = x
+        self.y = y
         self.score = score
-
+        self.size = size
+        self.font = pygame.font.Font(None, self.size)
+        self.high_score = 0
     def display(self):
-        pygame.display.set_caption("Shoot. Score: " + str(self.score))
-    def draw_arena(self):
-        self.scoreboard.display()
+        display_high_score = self.font.render(" Best score " + str(self.high_score), True, color['white'])
+        display_score = self.font.render(' Score ' + str(self.score), True, color['white'])
+        display_surf.blit(display_score, (self.x, self.y))
+        display_surf.blit(display_high_score, (self.x, self.y + 20))
